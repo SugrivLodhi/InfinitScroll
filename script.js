8c1f632ee51105925e80d9027c75cd0a4c442822
Sugriv <sugrivmlvt@gmail.com>
2022-07-05 13:26:14 +0530

    infineScroll

---
+++ script.js
@@ -0,0 +1,37 @@
+const container =document.querySelector('.container');
+let limit =4
+let pageCount =1
+let postCount =1
+
+const getPost = async() =>{
+   const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${pageCount}`)
+   const data  = await response.json(); 
+  
+   let postData = data.map((curData,index) =>{
+    return `<div class="post">
+    <span>${pageCount++}</span>
+    <h3>${curData.title}</h3>
+    <p>${curData.body}</p>
+    </div>`
+    
+   })
+  container.insertAdjacentHTML('beforeend',postData);
+}
+
+getPost();
+
+const showData = () =>{
+    setTimeout(() =>{
+       pageCount++
+       getPost()
+    },1000)
+}
+window.addEventListener("scroll",() =>{
+    const {scrollHeight,scrollTop,clientHeight} =document.documentElement
+      if(scrollTop + clientHeight >= scrollHeight){
+        console.log("touch the border")
+        showData();
+      }
+})
+       
+
